dc107fb4847e6a836e3ad77bf4b0f2524474a787
Brian Jeong <bjjeong@umich.edu>
2017-12-13 08:24:29 -0800

    New PO# Generator

--- ARS Source Code/arke.ars/arke.ars.technicianportal/Services/Impl/PurchaseOrderService.cs
+++ ARS Source Code/arke.ars/arke.ars.technicianportal/Services/Impl/PurchaseOrderService.cs
@@ -94,10 +94,31 @@ namespace Arke.ARS.TechnicianPortal.Services.Impl
             //Using ticks as a way to have unique PO #'s. This does not "guarantee" a different number
             //in the case that two work orders are created at the EXACT same time, but the chance that 
             //happens is miniscule.
-            long time = DateTime.Now.Ticks;
-            String fileName = Convert.ToString(time);
-            
-             //fileName.Substring(fileName.Length - 15); This is an attempt to make that PO # shorter. However, this might create the problem of collisions in the future. No guarantees for unique ID's. 
+
+            //long time = DateTime.Now.Ticks;
+            //String fileName = Convert.ToString(time);
+
+            var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var numbers = "0123456789";
+            var stringChars = new char[8];
+            var random = new Random();
+
+            for (int i = 0; i < 2; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+            for (int i = 2; i < 6; i++)
+            {
+                stringChars[i] = numbers[random.Next(numbers.Length)];
+            }
+            for (int i = 6; i < 8; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
+
+            var fileName = new String(stringChars);
+
+            //fileName.Substring(fileName.Length - 15); This is an attempt to make that PO # shorter. However, this might create the problem of collisions in the future. No guarantees for unique ID's. 
             //We will keep it at current length for now. If we really need to shorten it, we can later. 
             //For new STD system we probably still need this feature. But a lot of the features in the technician portal can be removed.
             //var receiptBool = false;
